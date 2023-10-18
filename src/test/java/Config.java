423cec1668ff3fda2957a4c066c0c23b8abda394
Oskar <oskar.lypka@gmail.com>
2023-10-18 17:38:15 +0200

    Constants set up

---
+++ src/test/java/Config.java
@@ -0,0 +1,14 @@
+public class Config {
+
+    // Usernames
+    public interface Users {
+        String STANDARD_USER = "standard_user";
+        String LOCKED_OUT_USER = "locked_out_user";
+        String PROBLEM_USER = "problem_user";
+        String PREFORMENCE_GLITCH_USER = "performance_glitch_user";
+        String ERROR_USER = "error_user";
+        String VISUAL_USER = "visual_user";
+    }
+    // Password for each user
+    public static final String SECRET_SAUCE = "secret_sauce";
+}
